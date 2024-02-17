4b8ac52d0da3740921bab241bd1416c7d79e7295
hea7en <vhavunka@gmail.com>
2024-02-18 00:28:32 +0200

    test level & menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void GoToSceneByName(string name) => SceneManager.LoadScene(name);
+    public void ExitGame() => Application.Quit();
+}
